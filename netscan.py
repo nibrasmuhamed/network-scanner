71b50ab8c81589af014d305afd6b65903690ba9c
Nibras Muhammed <nibrasmn027@gmail.com>
2021-06-07 16:01:06 +0530

    v1.0

---
+++ netscan.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# Network scan using scapy.
+#
+# This python script using scapy module will scan your network and returns all the clients
+# within the network.
+# 
+# this script should run with python 2.7 or python 3. any way you need to install dependencies
+# for curresponding version
+#
+# you need two dependencies to run:
+#   1.scapy
+#   2.colorama
+#
+#               coded by: @nibrasmuhamed #Github
+#                       @nibras_muhamed #Instagram
+#
+#
+import scapy.all as scapy
+import argparse
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-t" , dist="target", help="Enter your gateway with subnet(for eg:192.168.1.1/24)")
+    option = parser.parse_args()
+    return option
+
+
+def scan(ip):
+    arp_req = scapy.ARP(pdst=ip)
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    arp_req_broadcast = broadcast/arp_req
+    answered_lst = scapy.srp(arp_req_broadcast, timeout=1, verbose=False)[0]
+    print(answered_lst.summary())
+    client_list = []
+
+    for packet in answered_lst:
+        client_dic = {"ip": packet[1].psrc, "mac": packet[1].hwsrc}
+        client_list.append(client_dic)
+
+    return client_list
+
+
+def print_all(result_list):
+    print("IP Address\t\t\tMac Adress\n-------------------------------------")
+    for client in result_list:
+        print(client["ip"] + "\t\t" + client["mac"])
+
+
+option = get_args()
+scan_res = scan(option.target)
+print_all(scan_res)
